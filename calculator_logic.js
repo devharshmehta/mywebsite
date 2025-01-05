7e27ec94a5408e6200e6d8aa3dda386b140d0d7a
Harsh Mehta <84407356+devharshmehta@users.noreply.github.com>
2025-01-05 21:00:34 +0530

    Add files via upload

---
+++ calculator_logic.js
@@ -0,0 +1,14 @@
+function inputvalidation() {
+    const input_taken = document.getElementById('user_input');
+    let replaced_input = input_taken.value;
+    replaced_input = replaced_input.replace(/[^0-9+-/*().]/g, '');
+    input_taken.value = replaced_input;
+}
+function calculate() {
+    const input = document.getElementById('user_input');
+    const string = input.value;
+    const result = eval(string);
+    const output = document.getElementById('for_user');
+    output.innerHTML = `${result}`
+    
+}
